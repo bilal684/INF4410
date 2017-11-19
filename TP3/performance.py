12c66f027f46a6d866222c5594d4147c0e5150e8
Bilal Le Beau Bonhomme <bilal.itani1234@gmail.com>
2017-11-18 19:58:58 -0500

    Removed useless files and added readme and test performance script

--- TP3/performance.py
+++ TP3/performance.py
@@ -1,17 +1,20 @@
 from threading import Thread
 from urllib.request import urlopen
 from time import sleep
-import sys
+import sys, time
+
 def main(ip, port):
 	threads = []
+	start = time.time()
 	for i in range(0,40):
 		t = Thread(target=callRequestToServer, args=["http://"+ ip + ":" + port])
 		t.start()
 		threads.append(t)
-		sleep(0.1)
+		sleep(0.5)
 	for t in threads:
 		t.join()
-
+	end = time.time()
+	print("Execution time : " + str(end - start) + " seconds")
 def callRequestToServer(url):
 	return urlopen(url)
 	
---
+++ TP3/readme.txt
@@ -0,0 +1,10 @@
+performance.py est le script permettant d'effectuer les tests de performances.
+
+*****Il est necessaire d'avoir Python 3.X d'installer afin d'utiliser le script.*****
+
+Usage : python ./performance.py <IP Address of web server> 8000
+*Ici, 8000 represente le port des serveurs.
+
+Ainsi, pour un serveur (ou load balancer) ayant l'adresse 132.207.12.102, le script s'execute comme suit :
+
+python .\performance.py 132.207.12.102 8000
